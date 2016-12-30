b336140b9c8b43c08c2bc5f32c49140566e84382
vagrant <vagrant@homestead>
2016-12-30 05:45:48 +0000

    finish-filling-layout-style

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', 'StaticPagesController@home');//get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');//get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');//get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');//get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help')->name('help');//get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about')->name('about');//get('/about', 'StaticPagesController@about');
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="brand-icon" src="http://avatar.csdn.net/7/2/8/1_zhang6xin6yu.jpg">
+            <a href="http://my.csdn.net/zhang6xin6yu?locationNum=0&fps=1">
+                驴爱吃山楂 | 创造不息，交付不止
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{route('about')}}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
