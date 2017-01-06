f0b2d3d9078d084b11fcba4ee7282af56122c57b
vagrant <vagrant@homestead>
2017-01-06 10:30:52 +0000

    Add account activations

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,13 +8,16 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Auth;
+
 class SessionsController extends Controller
 {
-    public function __construct(){
-        $this->middleware('guest',[
-            'only'=>['create']
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
         ]);
     }
+
     public function create()
     {
         return view('sessions.create');
@@ -32,22 +35,30 @@ class SessionsController extends Controller
             'password' => $request->password
         ];
 
-        if (Auth::attempt($credentials,$request->has('remember'))) {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             /*Auth::attempt() 方法可接收两个参数，
             第一个参数为需要进行用户身份认证的数组，
             第二个参数为是否为用户开启『记住我』功能的布尔值*/
             //登录成功后的相关操作
-            session()->flash('success','欢迎回来！');
-            return redirect()->intended(route('users.show',[Auth::user()]));
+            if (Auth::user()->activated) {
+                session()->flash('success', '欢迎回来！');
+                return redirect()->intended(route('users.show', [Auth::user()]));
+            } else {
+                Auth::logout();
+                session()->flash('warning', '你的账号未激活，请检查邮箱中得注册邮件进行激活。');
+                return redirect('/');
+            }
         } else {
             //登录失败后的相关操作
-            session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back();
         }
     }
-    public function destroy(){
+
+    public function destroy()
+    {
         Auth::logout();
-        session()->flash('success','您已成功退出！');
+        session()->flash('success', '您已成功退出！');
         return redirect('login');
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Auth;
+use Mail;
 
 class UsersController extends Controller
 {
@@ -50,9 +51,9 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => $request->password,
         ]);
-        Auth::login($user);
-        session()->flash('success', '注册成功，在这开启您的新旅程吧~');
-        return redirect()->route('users.show', [$user]);
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到您的注册邮箱上，请注意查收。');
+        return redirect('/');
     }
 
     public function edit($id)
@@ -80,11 +81,34 @@ class UsersController extends Controller
 
         return redirect()->route('users.show', $id);
     }
-    public function destroy($id){
+    public function destroy($id){//删除用户
         $user = User::findOrFail($id);
         $this->authorize('destroy',$user);
         $user->delete();
         session()->flash('success','成功删除用户！');
         return back();
+    }
+    public function sendEmailConfirmationTo($user){//发送邮件
+       $view = 'emails.confirm';
+        $data = compact('user');
+        $from = '583145128@qq.com';
+        $name = 'zhangxinyu';
+        $to = $user->email;
+        $subject = "感谢注册Sample应用！请确认你的邮箱。";
+
+        Mail::send($view,$data,function($message) use ($from,$name,$to,$subject) {
+            $message->from($from,$name)->to($to)->subject($subject);
+        });
+    }
+    public function confirmEmail($token){//激活邮箱
+        $user = User::where('activation_token',$token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token= null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success','恭喜你，激活成功！');
+        return redirect()->route('users.show',[$user]);
     }
 }
